d87b891e233d9b9a5cc43e3f74c86b341eb05461
pythshoma228 <32521952+pythshoma228@users.noreply.github.com>
2018-12-14 22:57:37 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    constructor(props){
+        
+        super(props);
+        this.state = {date: new Date()};
+    }
+    render(){
+   return (
+        <div>
+            <h1>Hello world</h1>
+            <h2>it's {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+    );
+  }
+}
+
+export default App;
