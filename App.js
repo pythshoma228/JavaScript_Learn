c1f9194d4128277d76fb2d12c66da8320fb6b6ea
pythshoma228 <sh.magomedov05@mail.ru>
2018-12-19 16:45:05 +0300

    nice

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 class App extends Component {
     constructor(props){
-        
         super(props);
         this.state = {date: new Date()};
     }
@@ -12,6 +11,10 @@ class App extends Component {
         <div>
             <h1>Hello world</h1>
             <h2>it's {this.state.date.toLocaleTimeString()}.</h2> // Create date
+        </div>
+
+        <div>
+        	<pre>You very nice</pre>
         </div>
     );
   }
