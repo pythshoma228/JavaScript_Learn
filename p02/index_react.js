8656af0bc1728f5e64bf4aee0ac1db4ae8bb0232
ShMg <32521952+pythshoma228@users.noreply.github.com>
2019-03-09 22:30:57 +0300

    Update index_react.js

--- p02/index_react.js
+++ p02/index_react.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Component extends React.Component{
     constructor(props){
         super(props);
@@ -20,33 +19,6 @@ class Component extends React.Component{
         );
     }
 }
-
 ReactDOM.render(
 	<Component />,
-=======
-class Component extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-        this.state = {counter: 0};
-}
-
-    handleClick(){
-        this.setState({counter: ++this.state.counter});
-        //alert("hello world");
-    }
-
-    render(){
-
-        return (
-            <div>
-                <button onClick={this.handleClick}>Clicked {this.state.counter}</button>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(
-	<Component />,
->>>>>>> bec95967d3af91dcb5524a944d67e8bd82811c59
 	document.getElementById('root'));
